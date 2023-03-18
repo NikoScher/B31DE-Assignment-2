10f7e2aab0fb65e47414eb093208a772bc63e24a
Niko <niko.scher.26@gmail.com>
2023-03-18 18:37:02 +0000

    Timer Fix

--- Verilog/AHB_TIMER/AHBTIMER2023.v
+++ Verilog/AHB_TIMER/AHBTIMER2023.v
@@ -1,14 +1,5 @@
-// Very simple timer (counter) February 2023
-//
-// 32-bit register increments at rate of prescaled HCLK.
-// value of register is assigned to HRDATA.
-// Any address 0x52xxxxxx will switch the HRDATA output of this
-// subordinate to the AHBLITE bus multiplexer output.
-// These address range is set in, and consistency must be 
-// maintained between, files AHBDCD.v and AHBLITE_SYS.v 
-// AHBLITE bus address, control signals, and HSEL are ignored by
-// this module which is a sloppy means of implementation.
-// HRESETn will zero counter.
+
+// Insert header comments
 
 module AHBTIMER(
   input wire HCLK,
@@ -26,9 +17,11 @@ module AHBTIMER(
   output reg timer_irq
 );
 
-  //`define LIMITV_ADDR   32'h5200_0000
-  //`define CURRENTV_ADDR 32'h5200_0004
-  //`define CONTROL_ADDR  32'h5200_0008
+  `define LIMITV_ADDR   32'h5200_0000
+  `define CURRENTV_ADDR 32'h5200_0004
+  `define CONTROL_ADDR  32'h5200_0008
+
+  reg timer_irq_next;
 
   reg [31:0]  rHADDR;
   reg [31:0]  rHWDATA;
@@ -38,102 +31,69 @@ module AHBTIMER(
   reg [31:0]  rCURRENTV;
   reg [3:0]   rCONTROL;
 
-  reg timer_irq_next;
-
   //Prescaled clk signals (HCLK/16)
-  wire clk16;
+  wire CLK16;
+  reg rCLK16;
 
   //Generate prescaled clk ticks
   prescaler uprescaler16(
     .inclk(HCLK),
-    .outclk(clk16)
+    .outclk(CLK16)
   );
 
-  always @(posedge HCLK || negedge HRESETn) begin
+  always @(posedge CLK16)
+    rCLK16 = 1'b1;
+
+  always @(negedge CLK16)
+    rCLK16 = 1'b0;
+
+  always @(posedge HCLK or negedge HRESETn) begin
     if(!HRESETn) begin
-      rHADDR	  <= 32'h0;
-      rHWDATA	  <= 32'h0;
+      rHADDR    <= 32'h0;
+      rHWDATA   <= 32'h0;
       rHRDATA   <= 32'h0;
 
-      rLIMITV   <= 32'h0;
+      rLIMITV   <= 32'hffffffff;
       rCURRENTV <= 32'h0;
-      rCONTROL  <= 4'b0001;
+      rCONTROL  <= 4'b0101;
     end
 
     if (HSEL) begin
       rHADDR	<= HADDR;
       rHWDATA	<= HWDATA;
       if (HWRITE) begin
-        if (rHADDR == 32'h5200_0000) begin
+        if (rHADDR == LIMITV_ADDR)
           rLIMITV <= rHWDATA;
-        end
-        if (rHADDR == 32'h5200_0008) begin
+        if (rHADDR == CONTROL_ADDR)
           rCONTROL <= rHWDATA[3:0];
-        end
       end
     end
   end
   
   always @(posedge HCLK) begin
-    if (HSEL && HREADY) begin
-      rHRDATA <= rCURRENTV
-    end
+    if (HSEL && HREADY)
+      rHRDATA <= rCURRENTV;
 
-    if ((rCONTROL & 4'b1000) == 4'b0000) begin
-      if ((rCONTROL & 4'b0001) == 4'b0001) begin
+    if ((rCONTROL & 4'b0001) == 4'b0001) begin
+      if ((((rCONTROL & 4'b1000) == 4'b1000) && (rCLK16 == 1'b1)) || (((rCONTROL & 4'b1000) == 4'b0000) && (rCLK16 == 1'b0))) begin
         if ((rCONTROL & 4'b0010) == 4'b0010) begin
           rCURRENTV <= rCURRENTV + 1;
-          if (((rCONTROL & 4'b0100) == 4'b0100) && (rCURRENTV == rLIMITV)) begin
-            //Interupt
-            rCURRENTV <= 32'h0;
-          end
-          if (((rCONTROL & 4'b0100) == 4'b0000) && (rCURRENTV == 32'hffffffff)) begin
-            //Interupt
+          if ((((rCONTROL & 4'b0100) == 4'b0100) && (rCURRENTV == rLIMITV)) && (((rCONTROL & 4'b0100) == 4'b0000) && (rCURRENTV == 32'hffffffff))) begin
+            //Interupt cause hit limit
             rCURRENTV <= 32'h0;
           end
         end
         else begin
           rCURRENTV <= rCURRENTV - 1;
           if (rCURRENTV == 32'h0) begin
-            //Interupt
-            if ((rCONTROL & 4'b0100) == 4'b0100) begin
+            //Interupt cause hit limit
+            if ((rCONTROL & 4'b0100) == 4'b0100)
               rCURRENTV <= rLIMITV;
-            end
-            else begin
+            else
               rCURRENTV <= 32'hffffffff;
-            end
           end
         end
-      end
-    end
-  end
-
-  always @(posedge clk16) begin
-    if ((rCONTROL & 4'b1000) == 4'b1000) begin
-      if ((rCONTROL & 4'b0001) == 4'b0001) begin
-        if ((rCONTROL & 4'b0010) == 4'b0010) begin
-          rCURRENTV <= rCURRENTV + 1;
-          if (((rCONTROL & 4'b0100) == 4'b0100) && (rCURRENTV == rLIMITV)) begin
-            //Interupt
-            rCURRENTV <= 32'h0;
-          end
-          if (((rCONTROL & 4'b0100) == 4'b0000) && (rCURRENTV == 32'hffffffff)) begin
-            //Interupt
-            rCURRENTV <= 32'h0;
-          end
-        end
-        else begin
-          rCURRENTV <= rCURRENTV - 1;
-          if (rCURRENTV == 32'h0) begin
-            //Interupt
-            if ((rCONTROL & 4'b0100) == 4'b0100) begin
-              rCURRENTV <= rLIMITV;
-            end
-            else begin
-              rCURRENTV <= 32'hffffffff;
-            end
-          end
-        end
+        // Interupt cause value changed
       end
     end
   end
